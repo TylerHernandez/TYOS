2496e420ba93747152b138db8fe72d16c40604a7
Tyler Hernandez <tylerh6201@gmail.com>
2022-09-17 17:25:50 -0400

    Adds special character key functionality
    Adds special character key functionality through large switch statement and rearranging. Could be optimized for better performance through less comparisons-- will finish if remains enough time.

--- distrib/os/deviceDriverKeyboard.js
+++ distrib/os/deviceDriverKeyboard.js
@@ -28,24 +28,167 @@ var TSOS;
             var isShifted = params[1];
             _Kernel.krnTrace("Key code:" + keyCode + " shifted:" + isShifted);
             var chr = "";
-            // Check to see if we even want to deal with the key that was pressed.
-            if ((keyCode >= 65) && (keyCode <= 90)) { // letter
-                if (isShifted === true) {
-                    chr = String.fromCharCode(keyCode); // Uppercase A-Z
-                }
-                else {
-                    chr = String.fromCharCode(keyCode + 32); // Lowercase a-z
-                }
-                // TODO: Check for caps-lock and handle as shifted if so.
-                _KernelInputQueue.enqueue(chr);
-            }
-            else if (((keyCode >= 48) && (keyCode <= 57)) || // digits
-                (keyCode == 32) || // space
-                (keyCode == 13)) { // enter
+            if (keyCode == 8) { // Backspace
                 chr = String.fromCharCode(keyCode);
+            }
+            else if (keyCode == 13) { // Enter
+                chr = String.fromCharCode(keyCode);
+            }
+            else if (keyCode == 32) { // Space
+                chr = String.fromCharCode(keyCode);
+            }
+            else if (isShifted && (keyCode >= 65 && keyCode <= 90)) { //Letter
+                chr = String.fromCharCode(keyCode); // Uppercase A-Z
+            }
+            else if (!isShifted && (keyCode >= 48) && (keyCode <= 57)) { // Digits
+                chr = String.fromCharCode(keyCode);
+            }
+            else if (keyCode >= 65 && keyCode <= 90) { //Letter
+                chr = String.fromCharCode(keyCode + 32); // Lowercase a-z
+            }
+            else if (isShifted) {
+                switch (keyCode) {
+                    // Shifted special characters.
+                    case 48: {
+                        chr = ")";
+                        break;
+                    }
+                    case 49: {
+                        chr = "!";
+                        break;
+                    }
+                    case 50: {
+                        chr = "@";
+                        break;
+                    }
+                    case 51: {
+                        chr = "#";
+                        break;
+                    }
+                    case 52: {
+                        chr = "$";
+                        break;
+                    }
+                    case 53: {
+                        chr = "%";
+                        break;
+                    }
+                    case 54: {
+                        chr = "^";
+                        break;
+                    }
+                    case 55: {
+                        chr = "&";
+                        break;
+                    }
+                    case 56: {
+                        chr = "*";
+                        break;
+                    }
+                    case 57: {
+                        chr = "(";
+                        break;
+                    }
+                    case 186: {
+                        chr = ":";
+                        break;
+                    }
+                    case 187: {
+                        chr = "+";
+                        break;
+                    }
+                    case 188: {
+                        chr = "<";
+                        break;
+                    }
+                    case 189: {
+                        chr = "_";
+                        break;
+                    }
+                    case 190: {
+                        chr = ">";
+                        break;
+                    }
+                    case 191: {
+                        chr = "?";
+                        break;
+                    }
+                    case 192: {
+                        chr = "~";
+                        break;
+                    }
+                    case 219: {
+                        chr = "{";
+                        break;
+                    }
+                    case 220: {
+                        chr = "|";
+                        break;
+                    }
+                    case 221: {
+                        chr = "}";
+                        break;
+                    }
+                    case 222: {
+                        chr = "\"";
+                        break;
+                    }
+                }
+            } // ends isShifted 
+            else { // Not shifted keystroke.
+                switch (keyCode) {
+                    // Nonshifted special characters.
+                    case 186: {
+                        chr = ";";
+                        break;
+                    }
+                    case 187: {
+                        chr = "=";
+                        break;
+                    }
+                    case 188: {
+                        chr = ",";
+                        break;
+                    }
+                    case 189: {
+                        chr = "-";
+                        break;
+                    }
+                    case 190: {
+                        chr = ".";
+                        break;
+                    }
+                    case 191: {
+                        chr = "/";
+                        break;
+                    }
+                    case 192: {
+                        chr = "`";
+                        break;
+                    }
+                    case 219: {
+                        chr = "[";
+                        break;
+                    }
+                    case 220: {
+                        chr = "\\";
+                        break;
+                    }
+                    case 221: {
+                        chr = "]";
+                        break;
+                    }
+                    case 222: {
+                        chr = "'";
+                        break;
+                    }
+                }
+            }
+            if (chr.length > 0) {
                 _KernelInputQueue.enqueue(chr);
             }
-        }
+            // TODO: Check for caps-lock and handle as shifted if so.
+        } // ends function
     }
     TSOS.DeviceDriverKeyboard = DeviceDriverKeyboard;
 })(TSOS || (TSOS = {}));
